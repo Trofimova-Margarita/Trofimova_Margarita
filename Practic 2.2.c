f3616ee5577911e90aeec9902f8cec611e8c9f97
Trofimova-Margarita <trofimova.ms1998@yandex.ru>
2016-11-26 15:16:45 +0300

    Update Practic 2.2.c

--- Practic 2.2.c
+++ Practic 2.2.c
@@ -3,20 +3,20 @@
 int main()
 {
 	int b = 0, r = 0, i = 0;
-		printf("Guess the number I put forth\n");
-		r = 1 + rand() % 100;
-		do
-		{
-			scanf("%d", &b);
-			i = i ++;
-			if (b > r)
-				printf("My number is smaller\n");
-			else if (b < r)
-				printf("My number is bigger\n");
-				 else
-					 printf("Yes! You guessed %d attempts", i);
-		} 
-		while (b != r);
+	printf("Guess the number I put forth\n");
+	r = 1 + rand() % 100;
+	do
+	{
+	    scanf("%d", &b);
+	    i = i ++;
+	    if (b > r)
+		printf("My number is smaller\n");
+	    else if (b < r)
+		     printf("My number is bigger\n");
+		 else
+		     printf("Yes! You guessed %d attempts", i);
+	} 
+	while (b != r);
 	getch();
 	return 0;
 }
