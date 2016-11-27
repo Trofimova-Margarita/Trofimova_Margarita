1ec4570cdb2d22217a499ed097b01b7b60bd0651
Trofimova-Margarita <trofimova.ms1998@yandex.ru>
2016-11-27 17:22:37 +0300

    Create Practic 3.1.c

---
+++ Practic 3.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	char str[120];
+	int d = 0;
+	printf("Enter the string:\n");
+	scanf("%[^\n]s", &str);
+	for (int i = 1; str[i]!='\0'; i++)
+	    if (str[i]== ' ')
+	        d++;
+	printf("You entered %d words", d + 1);
+	getch();
+	return 0;
+}
