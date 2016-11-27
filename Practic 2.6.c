d604e688bc9efd730d1b8b1d3d2fccd164c9f439
Trofimova-Margarita <trofimova.ms1998@yandex.ru>
2016-11-27 18:40:33 +0300

    Update Practic 2.6.c

--- Practic 2.6.c
+++ Practic 2.6.c
@@ -7,10 +7,10 @@ int main()
 	gets(str);
 	for (int i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] != ' ')
-			printf("%c", str[i]);
-		if (str[i] != ' ' && str[i + 1] == ' ')
-			printf("%c", str[i + 1]);
+	    if (str[i] != ' ')
+		printf("%c", str[i]);
+	    if (str[i] != ' ' && str[i + 1] == ' ')
+		printf("%c", str[i + 1]);
 	}
 	getch();
 	return 0;
