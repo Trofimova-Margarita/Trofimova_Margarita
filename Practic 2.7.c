a7b620e8e7df776c6af4a31921652c576a7cf7e0
Trofimova-Margarita <trofimova.ms1998@yandex.ru>
2016-11-29 22:28:06 +0300

    Create Practic 2.7.c

---
+++ Practic 2.7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define map_size 256
+int main()
+{
+	int map[map_size] = { 0 };
+	char str[120];
+	printf("Enter the string:\n");
+	gets(str);
+	for (int i = 0; i < strlen(str); i++)
+	    map[str[i]]++;
+	printf("Occurrence of characters in the string:\n");
+	for (int i = 1; i < map_size; i++)
+	{
+	    if (map[i])
+	  printf("'%c' -> %d\n", (char)i, map[i]);
+	}
+	getch();
+	return 0;
+}
