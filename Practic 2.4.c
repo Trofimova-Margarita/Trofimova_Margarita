c4f947d992383818e75a84ad337ebc53513dbbc7
Trofimova-Margarita <trofimova.ms1998@yandex.ru>
2016-12-03 13:50:26 +0300

    Update Practic 2.4.c

--- Practic 2.4.c
+++ Practic 2.4.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
+	const int A=48, B=65, C=97;
 	int r1 = 0, r2 = 0, r3 = 0;
 	char string[50];
 	srand(time(NULL));
@@ -11,19 +12,19 @@ int main()
 	    {
 		case 0:
 		{
-		    r1 = 48 + rand() % 10;
+		    r1 = A + rand() % 10;
 		    string[i] = (char)r1;
 		}
 		break;
 		case 1:
 		{
-		    r2 = 65 + rand() % 26;
+		    r2 = B + rand() % 26;
 		    string[i] = (char)r2;
 		}
 		break;
 		case 2:
 		{
-		    r3 = 97 + rand() % 26;
+		    r3 = C + rand() % 26;
 		    string[i]= (char)r3;
 		}
 		break;
@@ -32,12 +33,12 @@ int main()
 	printf("String before: %s\nString after:  ", string);
 	for (int i = 0; i < 40; i++)
 	{
-	    if ((string[i] >= (char)65 && string[i] <= (char)90) || (string[i] >= (char)97 && string[i] <= (char)122))
+	    if ((string[i] >= (char)B && string[i] <= (char)90) || (string[i] >= (char)C && string[i] <= (char)122))
 		printf("%c", string[i]);
 	}
 	for (int i = 0; i < strlen(string); i++)
 	{
-	    if (string[i] >= (char)48 && string[i] <= (char)57)
+	    if (string[i] >= (char)A && string[i] <= (char)57)
 		printf("%c", string[i]);
 	}
 	getch();
