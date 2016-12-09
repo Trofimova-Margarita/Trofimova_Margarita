a6f51a813c5fd84a1a4363081e4c118c55077e73
Trofimova-Margarita <trofimova.ms1998@yandex.ru>
2016-12-09 23:26:53 +0300

    Update Practic 3.5.c

--- Practic 3.5.c
+++ Practic 3.5.c
@@ -9,29 +9,29 @@ int main()
 	int_arr[i]= -99 + rand() % 199;
 	printf("%d ", int_arr[i]);
 	}
-	for (int i =SIZE; i >=0; i--)
+	for (int i = SIZE; i >= 0; i--)
 	{
-		if (int_arr[i] < 0)
-		{
-			p_p[0] = int_arr[i];
-			num_i[0] = i;
-		}	
+	    if (int_arr[i] < 0)
+	    {
+		p_p[0] = int_arr[i];
+		num_i[0] = i;
+	    }	
 	}
 	for (int i =0; i<SIZE ; i++)
 	{
-		if (int_arr[i]> 0)
-		{
-			p_p[1] = int_arr[i];
-			num_i[1] = i;
-		}
+	    if (int_arr[i] > 0)
+	    {
+		p_p[1] = int_arr[i];
+		num_i[1] = i;
+	    }
 	}
-	printf("\nThe first a negative number: %d",p_p[0]);
-	printf("\nThe last a positive number: %d", p_p[1]);
+	printf("\nThe first negative number: %d",p_p[0]);
+	printf("\nThe last positive number: %d", p_p[1]);
 	x = num_i[0]+1;
 	for (int i = 1; i <(num_i[1]-num_i[0]); i++)
 	{
-		sum= sum + int_arr[x];
-		x++;
+	    sum= sum + int_arr[x];
+	    x++;
 	}
 	printf("\nThe sum diapason: %d",sum);
 	getch();
