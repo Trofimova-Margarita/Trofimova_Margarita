075a9a81ed6ae6bd59e9cd47f3ec8ad92d7cd098
Trofimova-Margarita <trofimova.ms1998@yandex.ru>
2016-12-09 21:45:57 +0300

    Update Practic 3.4.c

--- Practic 3.4.c
+++ Practic 3.4.c
@@ -8,15 +8,15 @@ int main()
 	a = strlen(str) / 4;
 	for (int c = 0; c < a; c++)
 	{
-		d = d + ((int)str[i] - 48)*pow(10, n) + ((int)str[i + 1] - 48)*pow(10, n - 1) + ((int)str[i + 2] - 48)*pow(10, n - 2) + ((int)str[i + 3] - 48)*pow(10, n - 3);
-		i = i + 4;
+	    d = d + ((int)str[i] - 48)*pow(10, n) + ((int)str[i + 1] - 48)*pow(10, n - 1) + ((int)str[i + 2] - 48)*pow(10, n - 2) + ((int)str[i + 3] - 48)*pow(10, n - 3);
+	    i = i + 4;
 	}
 	n = strlen(str) - 4 * a;
-	do 
+	do
 	{
-		d = d + ((int)str[i] - 48)*pow(10, n - 1);
-		n--;
-		i++;
+	    d = d + ((int)str[i] - 48)*pow(10, n - 1);
+	    n--;
+	    i++;
 	} while (n != 0);
 	printf("The sum of four-digit numbers: %d", d);
 	getch();
