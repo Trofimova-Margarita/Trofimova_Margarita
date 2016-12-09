29ef0c80ff037acdfe566e47d13880ec5b5ddb0b
Trofimova-Margarita <trofimova.ms1998@yandex.ru>
2016-12-09 20:26:54 +0300

    Update Practic 3.3.c

--- Practic 3.3.c
+++ Practic 3.3.c
@@ -10,21 +10,21 @@ int main()
 	{
 	    if (isalpha(string[i]))
 	    {
-	  word_inc++;
+		word_inc++;
 		if (string[i - 1] == ' ' || string[i - 1] == 0)
+		{
+		    if (word[1] < word_inc)
 		    {
-		  if (word[1] < word_inc)
-				{
-					word[0] = i;
-					word[1] = word_inc;
-				}
-				word_inc = 0;
-			}
+			word[0] = i;
+			word[1] = word_inc;
+		    }
+		    word_inc = 0;
 		}
+	    }
 	}
 	for (int i = word[0]; i != word[0] + word[1]; i++)
 	{
-		putchar(string[i]);
+	    putchar(string[i]);
 	}
 	printf("\nIts length is %d characters.\n", word[1]);
 	getch();
