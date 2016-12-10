dedc08bd56def1599708a8ce9972022528d84d54
Trofimova-Margarita <trofimova.ms1998@yandex.ru>
2016-12-10 23:11:37 +0300

    Update Practic 3.6.c

--- Practic 3.6.c
+++ Practic 3.6.c
@@ -6,34 +6,34 @@ int main()
 	srand(time(NULL));
 	for (int i = 0; i <SIZE; i++)
 	{
-		int_arr[i] = -99 + rand() % 199;
-		printf("%d ", int_arr[i]);
+	    int_arr[i] = -99 + rand() % 199;
+	    printf("%d ", int_arr[i]);
 	}
 	min_max[0] = int_arr[0];
 	min_max[1] = int_arr[0];
 	for (int i = 0; i <SIZE; i++)
 	{
-		if (int_arr[i] < min_max[0])
-		{
-			min_max[0] = int_arr[i];
-			num_i[0] = i;
-		}
+	    if (int_arr[i] < min_max[0])
+	    {
+		min_max[0] = int_arr[i];
+		num_i[0] = i;
+	    }
 	}
 	for (int i = 0; i<SIZE; i++)
 	{
-		if (int_arr[i]> min_max[1])
-		{
-			min_max[1] = int_arr[i];
-			num_i[1] = i;
-		}
+	    if (int_arr[i]> min_max[1])
+	    {
+		min_max[1] = int_arr[i];
+		num_i[1] = i;
+	    }
 	}
 	printf("\nThe min number: %d", min_max[0]);
 	printf("\nThe max number: %d", min_max[1]);
 	x = num_i[0] + 1;
 	for (int i = 1; i <(num_i[1] - num_i[0]); i++)
 	{
-		sum = sum + int_arr[x];
-		x++;
+	    sum = sum + int_arr[x];
+	    x++;
 	}
 	printf("\nThe sum diapason: %d", sum);
 	getch();
