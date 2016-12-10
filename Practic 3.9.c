35d75912114584898e5f5fcb6b7e28540d977d11
Trofimova-Margarita <trofimova.ms1998@yandex.ru>
2016-12-11 01:29:54 +0300

    Update Practic 3.9.c

--- Practic 3.9.c
+++ Practic 3.9.c
@@ -2,24 +2,24 @@
 int main()
 {
 	char str[20];
-	printf("Enter\n");
+	printf("Enter the string\n");
 	gets(str);
 	int a = 1, b = 0, c = 0;
 	for (int i = 0; i < strlen(str) - 1; i++)
 	{
-		if (str[i] == str[i + 1])
-			a = a + 1;
-		else if ((a != 1) && (b < a))
-		{
-			b = a;
-			c = i;
-			a = 1;
-		}
+	    if (str[i] == str[i + 1])
+		a = a + 1;
+	    else if ((a != 1) && (b < a))
+	    {
+		b = a;
+		c = i;
+		a = 1;
+	    }
 	}
-	printf("Lenght: %d\n", b);
-	printf("Max str = ");
+	printf("Length: %d\n", b);
+	printf("The longest string: ");
 	for (int x = (c - b)+1; x <= c; x++)
-		printf("%c", str[x]);
+	    printf("%c", str[x]);
 	getch();
 	return 0;
 }
