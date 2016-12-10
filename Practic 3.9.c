b3ca5923fdc57a646bc63e08688d0a709eeb5b79
Trofimova-Margarita <trofimova.ms1998@yandex.ru>
2016-12-11 01:25:47 +0300

    Create Practic 3.9.c

---
+++ Practic 3.9.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+	char str[20];
+	printf("Enter\n");
+	gets(str);
+	int a = 1, b = 0, c = 0;
+	for (int i = 0; i < strlen(str) - 1; i++)
+	{
+		if (str[i] == str[i + 1])
+			a = a + 1;
+		else if ((a != 1) && (b < a))
+		{
+			b = a;
+			c = i;
+			a = 1;
+		}
+	}
+	printf("Lenght: %d\n", b);
+	printf("Max str = ");
+	for (int x = (c - b)+1; x <= c; x++)
+		printf("%c", str[x]);
+	getch();
+	return 0;
+}
