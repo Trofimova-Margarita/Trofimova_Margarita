e684a131087ebfc65acc329ae2f84fd64de27279
Trofimova-Margarita <trofimova.ms1998@yandex.ru>
2017-02-04 11:49:06 +0300

    Update Practic 5.4.c

--- Practic 5.4/Practic 5.4.c
+++ Practic 5.4/Practic 5.4.c
@@ -5,18 +5,24 @@
 #include <Windows.h>
 #include <stdbool.h>
 
-struct STRING {
+struct STRING
+{
 	char str[80];
 	char *pointers[10];
 	int wordcount;
 };
 typedef struct STRING STRING;
-void getWords(STRING *s) {
-	for (int i = 0; i<strlen(s->str) - 1; i++) {
+
+void getWords(STRING *s)
+{
+	for (int i = 0; i<strlen(s->str) - 1; i++)
+	{
 		s->pointers[s->wordcount] = &s->str[i];
 		s->wordcount++;
-		for (int j = i;; j++) {
-			if (s->str[j] == ' ') {
+		for (int j = i;; j++)
+		{
+			if (s->str[j] == ' ')
+			{
 				i = j;
 				break;
 			}
@@ -26,33 +32,39 @@ void getWords(STRING *s) {
 	}
 }
 
-void printWord(STRING *s, int n) {
+void printWord(STRING *s, int n)
+{
 	char *now = s->pointers[n];
-	while (true) {
-		if (*now != ' ' && *now != '\0') {
+	while (true)
+	{
+		if (*now != ' ' && *now != '\0')
+		{
 			printf("%c", *now);
 			now++;
 		}
-		else {
+		else
+		{
 			printf("%c", ' ');
 			break;
 		}
 	}
 }
 
-void MixingPrint(STRING *s) {
-
+void MixingPrint(STRING *s)
+{
 	int *nomixednumbers = (int*)malloc(s->wordcount * sizeof(int));
 	int *mixednumbers = (int*)malloc(s->wordcount * sizeof(int));
-
-	for (int i = 0; i<s->wordcount; i++) {
+	for (int i = 0; i<s->wordcount; i++)
+	{
 		nomixednumbers[i] = i;
 	}
-
-	for (int i = 0; i<s->wordcount; i++) {
+	for (int i = 0; i<s->wordcount; i++)
+	{
 		int **unmixed = (int**)malloc((s->wordcount - i) * sizeof(int*));
-		for (int j = 0, n = 0; j<s->wordcount; j++, n++) {
-			if (nomixednumbers[j] != -1) {
+		for (int j = 0, n = 0; j<s->wordcount; j++, n++)
+		{
+			if (nomixednumbers[j] != -1)
+			{
 				unmixed[n] = &nomixednumbers[j];
 			}
 			else
@@ -63,34 +75,32 @@ void MixingPrint(STRING *s) {
 		*unmixed[r] = -1;
 		free(unmixed);
 	}
-
-	for (int i = 0; i<s->wordcount; i++) {
+	for (int i = 0; i<s->wordcount; i++)
+	{
 		printWord(s, mixednumbers[i]);
 	}
-
 	free(nomixednumbers);
 	free(mixednumbers);
 }
 
-int main() {
-
+int main()
+{
 	STRING string;
 	string.wordcount = 0;
-
 	int startval = time(NULL);
 	srand(startval);
-
 	FILE *file;
 	file = fopen("text.txt", "r");
-
 	if (file == NULL)
 	{
 		printf("Dont open");
 	}
-	else {
+	else
+	{
 		while (!feof(file))
 		{
-			if (fgets(string.str, 80, file)) {
+			if (fgets(string.str, 80, file))
+			{
 				string.wordcount = 0;
 				getWords(&string);
 				MixingPrint(&string);
