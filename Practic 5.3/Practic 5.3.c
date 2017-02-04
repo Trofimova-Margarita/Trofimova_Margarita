eb02f43688ff3c8553f2688e52f1859c3f93ad45
Trofimova-Margarita <trofimova.ms1998@yandex.ru>
2017-02-04 11:52:25 +0300

    Update Practic 5.3.c

--- Practic 5.3/Practic 5.3.c
+++ Practic 5.3/Practic 5.3.c
@@ -2,29 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-
 #define SIZE_TEN 10
-struct STRING {
+
+struct STRING
+{
 	char str[SIZE_TEN][80];
 	char *pword[SIZE_TEN][SIZE_TEN];
 	int wordsinstr[SIZE_TEN];
 	int strsize;
 };
+
 typedef struct STRING STRING;
 
-void FindWords(STRING *s) {
-	for (int i = 0; i<s->strsize; i++) {
+void FindWords(STRING *s)
+{
+	for (int i = 0; i<s->strsize; i++)
+	{
 		int wordsfind = 0;
 		bool nextword = true;
-		for (int j = 0;; j++) {
+		for (int j = 0;; j++)
+		{
 			if (s->str[i][j] == '\0')
 				break;
-			if (s->str[i][j] != ' ' && nextword == true) {
+			if (s->str[i][j] != ' ' && nextword == true)
+			{
 				s->pword[i][wordsfind] = &s->str[i][j];
 				wordsfind++;
 				nextword = false;
 			}
-			else if (s->str[i][j] == ' ' && nextword == false) {
+			else if (s->str[i][j] == ' ' && nextword == false)
+			{
 				nextword = true;
 			}
 		}
@@ -32,15 +39,19 @@ void FindWords(STRING *s) {
 	}
 }
 
-void GetWord(char w[80], char *p) {
+void GetWord(char w[80], char *p)
+{
 	int wsize = 0;
-	while (true) {
-		if (*p != NULL && *p != '\0') {
+	while (true)
+	{
+		if (*p != NULL && *p != '\0')
+		{
 			w[wsize] = *p;
 			p++;
 			wsize++;
 		}
-		else {
+		else
+		{
 			if (*p == '\0')
 				w[--wsize] = '\0';
 			else
@@ -50,11 +61,15 @@ void GetWord(char w[80], char *p) {
 	}
 }
 
-void RemoveSpaces(char *s) {
-	for (int i = 0; i<strlen(s); i++) {
+void RemoveSpaces(char *s)
+{
+	for (int i = 0; i<strlen(s); i++)
+	{
 		char *see = s + i;
-		if (*see == ' ') {
-			for (int j = i; j<strlen(s); j++) {
+		if (*see == ' ')
+		{
+			for (int j = i; j<strlen(s); j++)
+			{
 				char *first = s + j;
 				char *second = s + j + 1;
 				char a = *first;
@@ -66,45 +81,47 @@ void RemoveSpaces(char *s) {
 	}
 }
 
-void RandomMixed(char w[80]) {
+void RandomMixed(char w[80])
+{
 	int leight = strlen(w);
 	if (leight<4)
 		return;
 	char *nomixed = (char*)calloc(leight - 2, sizeof(char));
-
 	char *mixed = (char*)calloc(leight - 2, sizeof(char));
-
-	for (int i = 1; i<leight - 1; i++) {
+	for (int i = 1; i<leight - 1; i++)
+	{
 		nomixed[i - 1] = w[i];
 	}
-	for (int i = 0; i<leight - 2; i++) {
+	for (int i = 0; i<leight - 2; i++)
+	{
 		int buffsize = leight - 2 - i;
-
 		char **unusedsymbols = (char**)malloc(buffsize * sizeof(char*));
-		for (int j = 0, n = 0; j<leight - 1; j++, n++) {
-
+		for (int j = 0, n = 0; j<leight - 1; j++, n++)
+		{
 			if (nomixed[j] != ' ')
 				unusedsymbols[n] = &nomixed[j];
 			else
 				n--;
 		}
 		int r = rand() % buffsize;
-
 		mixed[i] = *unusedsymbols[r];
-
 		*unusedsymbols[r] = ' ';
 		free(unusedsymbols);
 	}
-	for (int i = 0; i<leight - 2; i++) {
+	for (int i = 0; i<leight - 2; i++)
+	{
 		w[i + 1] = mixed[i];
 	}
 	free(nomixed);
 	free(mixed);
 }
 
-void MixedPrint(STRING *s) {
-	for (int i = 0; i<s->strsize; i++) {
-		for (int j = 0; j<s->wordsinstr[i]; j++) {
+void MixedPrint(STRING *s)
+{
+	for (int i = 0; i<s->strsize; i++)
+	{
+		for (int j = 0; j<s->wordsinstr[i]; j++)
+		{
 			char word[80];
 			GetWord(word, s->pword[i][j]);
 			RandomMixed(word);
@@ -114,36 +131,35 @@ void MixedPrint(STRING *s) {
 	}
 }
 
-int main() {
-
+int main()
+{
 	STRING string;
 	string.strsize = 0;
-
 	int rvalue = (int)time(NULL);
 	srand(rvalue);
-
 	FILE *file;
 	file = fopen("text.txt", "r");
-
 	if (file == NULL)
 	{
 		printf("Dont open");
 	}
-	else {
+	else
+	{
 		while (!feof(file))
 		{
-			if (fgets(string.str[string.strsize], 120, file)) {
+			if (fgets(string.str[string.strsize], 120, file))
+			{
 				string.strsize++;
 			}
 		}
-		for (int i = 0; i<string.strsize; i++) {
+		for (int i = 0; i<string.strsize; i++)
+		{
 			printf("%s\n", string.str[i]);
 		}
 		printf("* * *\n");
 		FindWords(&string);
 		MixedPrint(&string);
 	}
-
 	getch();
 	return 0;
 }
